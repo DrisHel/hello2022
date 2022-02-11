e412363bf6dfc1ef5ab55fe7cd3df2f995ad2485
Doris Nunes <doris90nunes@gmail.com>
2022-02-10 23:34:50 -0300

    fix(correction)json code passed to javascript.

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello World!",
-    })
-  );
+  res.end("Primeira aplicação do ano de 2022!");
 });
 
 server.listen(8000, () => console.log("Servidor rodando ma porta 8000!"));
